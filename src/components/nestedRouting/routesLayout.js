025715806810ad6a7db9b4676d6f44d113cd0318
amlp33 <sharmarahul965797@gmail.com>
2022-09-18 11:39:11 +0530

    routing and lazy load updates

--- src/components/nestedRouting/routesLayout.js
+++ src/components/nestedRouting/routesLayout.js
@@ -3,10 +3,14 @@ import { Outlet, Link } from "react-router-dom";
 
 const RoutesLayout = () => {
   return (
-    <div>
-      <p>all the links</p>
+    <div
+      style="
+  min-width: 600px;
+  max-width: 850px;"
+    >
       <Link to="nr/abc">profile</Link>&nbsp;&nbsp;
       <Link to="nr/efg">lazyload</Link>
+      <hr style="border:none; border-bottom:1px solid lightgray" />
       <Outlet />
     </div>
   );
